80ccc22e03b760e6687928099922b7880b491257
Kate Tran <93722899+KateLTran@users.noreply.github.com>
2022-06-29 10:58:11 -0400

    Initial commit

---
+++ config.R
@@ -0,0 +1,18 @@
+
+
+############# file paths ############# 
+
+#path to abcd Restricted Access folder
+main_abcd_path = "~/Box Sync/"
+abcd_box_path = "2. Barzi Lab - Restricted Access/2-ABCD Data Files/"
+abcd_data_path = "ABCD data/"
+prs_box_path = "Ran_Barzilay/"
+abcd_version = "4.0/"
+abcd_covid = "covid19/"
+
+abcd_files_path = paste0(main_abcd_path, abcd_box_path, abcd_data_path, abcd_version)
+abcd_covid_r1_files_path = paste0(main_abcd_path, abcd_box_path, abcd_data_path, abcd_covid, "1_Data_Release/")
+abcd_covid_r2_files_path = paste0(main_abcd_path, abcd_box_path, abcd_data_path, abcd_covid, "2_Data_Release/")
+abcd_covid_r3_files_path = paste0(main_abcd_path, abcd_box_path, abcd_data_path, abcd_covid, "3_Data_Release/")
+additional_files_path = paste0(main_abcd_path, abcd_box_path,"Additional files/")
+prs_files_path = paste0(main_abcd_path, prs_box_path)
